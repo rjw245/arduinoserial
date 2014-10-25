4fdfeed52031f18e56e9e735da6ebea33d35347b
Riley <riley@ubuntu.(none)>
2014-10-24 21:43:15 -0400

    Added arduino functionality

--- arduinoSample.py
+++ arduinoSample.py
@@ -5,8 +5,9 @@ baudrate = 9600
 arduino = serial.Serial(port,baudrate)
 
 while True:
-	userin = raw_input("Send: ")
-	arduino.write(userin+"\n")
-	time.sleep(0.1)
-	while arduino.inWaiting()>0 :
-		print arduino.readline()
+	userin = raw_input("Send: ")   # Get user input from terminal
+	userin = userin.strip()        # Get rid of whitespace characters
+	arduino.write(userin)          # Send string over serial connection
+	time.sleep(0.01)               # Wait a bit for the response
+	while arduino.inWaiting()>0 : # If there is a response waiting...
+		print arduino.readline()  # then print it
--- arduinoserial/arduinoserial.ino
+++ arduinoserial/arduinoserial.ino
@@ -1,13 +1,25 @@
+int led = 13;
+
 void setup(){
   int baudrate = 9600;
   Serial.begin(baudrate);
+  pinMode(led,OUTPUT);
 }
 
 void loop(){
   if(Serial.available()){
     char c = Serial.read();
     if(c=='1'){
-      Serial.println("Hello! I'm an Arduino!");
+      Serial.println("LED is on");
+      //Do whatever else
+      digitalWrite(led,HIGH);
+    }
+    else if(c=='2'){
+      Serial.println("LED is off");
+      digitalWrite(led,LOW);
+    }
+    else if(c=='3'){
+      Serial.println("Hello I'm an Arduino!");
     }
   }
 }
